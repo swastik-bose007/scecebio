21df167f005fbbf42e314b1ac8911ba62bc8efbb
swastik <deltaagents4@gmail.com>
2022-10-17 17:56:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const explore = document.getElementById('event');
+// const preLoader = document.getElementsByClassName('pre-Loader');
+document.querySelector('#event').addEventListener("click", function(){
+    document.querySelector('.popup').style.display = 'none';
+    // document.querySelector('.preloader').style.display = 'none';
+    document.querySelector('.pre-Loader').style.background = 'transparent';
+    document.querySelector('.pre-Loader').style.height = '0vh';
+
+});
+
+function facebook() {
+    window.location = "https://www.facebook.com/sceceaot/"
+}
+function instagram() {
+    window.location = "https://instagram.com/aot_sc_ece?igshid=YmMyMTA2M2Y="
+}
+function linkdin() {
+    window.location = "https://www.linkedin.com/company/student-s-chapter-of-iei-ece-academy-of-technology/"
+}
+function twitter() {
+    window.location = "https://twitter.com/IeiEce?t=O0w4dUffK0Zm2RIg5fsdAg&s=08"
+}
+function whatsapp() {
+    window.location = "https://chat.whatsapp.com/KPtqPU0aoLVEPdLxjXKpNT"
+}
+
+
+const newyear = new Date().getFullYear();
+document.getElementById('year').innerHTML = newyear;
